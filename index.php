1083f0ee2ec9d8dd136e6a9d45c08c0cfece38e8
JojenRWillis <JojenRWillis@outlook.com>
2018-02-11 18:40:40 +0000

    test change

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ $home = array_merge($defaultpage, $home);
 $home["navbar"][0]["current"][0]["raisin"] = "(current)";
 $home["header"][0]["title"] = "";
 $home["page_text"][0]["main"][0]["title"][0]["words"] = "About Human Ecology";
-$home["page_text"][0]["main"][0]["paragraph"][0]["content"] = "blahblahblahblahbala";
+$home["page_text"][0]["main"][0]["paragraph"][0]["content"] = "blhblahblahblahbala";
 $home["page_text"][0]["button"][0]["name"] = "Read More";
 $home = array_merge($card, $home);
 
