5116f2316eb2ef4f387184bcbe84d87855def589
Ember <falconsflight@riseup.net>
2018-02-12 19:59:46 +0000

    made image pages work

--- index.php
+++ index.php
@@ -110,8 +110,13 @@ $pets["page_text"][0]["main"][0]["paragraph"][0]["content"] = $lipsum;
 $image["title"] = "Image";
 $image = array_merge($defaultpage, $image);
 $image = array_merge($card, $image);
-$image["image_url"] = "http://placehold.it/750x500";
-$image["url"] = "/buy";
+$carrot = substr($_SERVER['REQUEST_URI'], 1);
+$all_carrots = (R::getAll("SELECT $carrot"));
+//print_r (R::getAll("SELECT watermark FROM photo WHERE identification = $carrot"));
+$current_photo_row = R::find("photo","identification = ?", [$carrot]);
+$current_photo_url = $current_photo_row[1]["watermark"];
+$image["image_url"] = $current_photo_url;
+$image["image_id"] = $carrot;
 $image["description"] = $lipsum;
 
 $contact["title"] = "Contact me";
